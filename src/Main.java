1cf344d19db8918c2fecb1acb51074ee79663603
Ani <ani-alaverdyan@mail.ru>
2022-08-07 19:26:08 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Book annaKarenina = new Book("Анна Каренина",  1878, levTolstoy);
+
+        annaKarenina.setPublicationYear(1895);
+
+        System.out.println(levTolstoy);
+        System.out.println(annaKarenina);
+
+        Author mariaRemark = new Author( "Мария", "Ремарк");
+        Book threeComrades = new Book( "Три товарища",1936, mariaRemark);
+
+        threeComrades.setPublicationYear(1942);
+        System.out.println(mariaRemark);
+        System.out.println(threeComrades);
+
+
     }
 }
